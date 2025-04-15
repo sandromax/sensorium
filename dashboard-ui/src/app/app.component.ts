d6fed903c0d1725f567bd75e26fdfad1fc99e5e1
sandromax <sandrondk@gmail.com>
2025-04-15 22:43:38 +0200

    Sensor Chart Component

--- dashboard-ui/src/app/app.component.ts
+++ dashboard-ui/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { SensorChartComponent } from './components/sensor-chart/sensor-chart.component';
+import { SensorTableComponent } from 'src/app/components/sensor-table/sensor-table.component';
 
 @Component({
     selector: 'app-root',
-    imports: [],
+    standalone: true,
+    imports: [SensorChartComponent, SensorTableComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
--- dashboard-ui/src/app/components/sensor-chart/sensor-chart.component.ts
+++ dashboard-ui/src/app/components/sensor-chart/sensor-chart.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { SensorService, SensorData } from 'src/app/services/sensor.service';
 
 @Component({
   selector: 'app-sensor-chart',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, NgxChartsModule],
   templateUrl: './sensor-chart.component.html',
-  styleUrl: './sensor-chart.component.scss'
+  styleUrls: ['./sensor-chart.component.scss']
 })
-export class SensorChartComponent {
+export class SensorChartComponent implements OnInit {
+  sensorId = 'sensor-1';
+  chartData: any[] = [];
 
+  view: [number, number] = [700, 300];
+
+  // Chart options
+  showXAxis = true;
+  showYAxis = true;
+  showLegend = false;
+  showXAxisLabel = true;
+  showYAxisLabel = true;
+  xAxisLabel = 'Час';
+  yAxisLabel = 'Значення';
+
+  constructor(private sensorService: SensorService) {}
+
+  ngOnInit(): void {
+    this.sensorService.getHistory(this.sensorId, '6h').subscribe(data => {
+
+      console.log('[SensorChart] Got data:', data);
+
+      this.chartData = [
+        {
+          name: this.sensorId,
+          series: data
+            .filter(d => !isNaN(d.value))
+            .map(d => ({
+            name: new Date(d.timestamp).toLocaleTimeString(),
+            value: d.value
+          }))
+        }
+      ];
+    });
+  }
 }
