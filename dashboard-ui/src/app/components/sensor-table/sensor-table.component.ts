48958bf12132e1742e669818ca7c078f1aca3c8c
sandromax <sandrondk@gmail.com>
2025-04-15 22:28:35 +0200

    Live update of the graph and table every 5 seconds

--- dashboard-ui/src/app/components/sensor-table/sensor-table.component.ts
+++ dashboard-ui/src/app/components/sensor-table/sensor-table.component.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { SensorService, SensorData } from 'src/app/services/sensor.service';
+import { CommonModule } from '@angular/common';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-sensor-table',
-  templateUrl: './sensor-table.component.html'
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sensor-table.component.html',
+  styleUrls: ['./sensor-table.component.scss']
 })
 export class SensorTableComponent implements OnInit {
   sensors: SensorData[] = [];
+  subscription!: Subscription;
 
   constructor(private sensorService: SensorService) {}
 
-  ngOnInit() {
-    this.sensorService.getLatest().subscribe(data => {
+  ngOnInit(): void {
+    this.loadData();
+
+    this.subscription = interval(5000).subscribe(() => {
+      this.loadData();
+    });
+  }
+
+  loadData(): void {
+    this.sensorService.getLatest().subscribe((data) => {
       this.sensors = data;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
