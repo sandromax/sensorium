26398754796ad11f41f41c8672d7074f47b0be4c
sandromax <sandrondk@gmail.com>
2025-04-15 22:53:11 +0200

    Live 5 sec in SensorChartComponent

--- dashboard-ui/src/app/components/sensor-chart/sensor-chart.component.ts
+++ dashboard-ui/src/app/components/sensor-chart/sensor-chart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { SensorService, SensorData } from 'src/app/services/sensor.service';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sensor-chart',
@@ -13,6 +14,7 @@ import { SensorService, SensorData } from 'src/app/services/sensor.service';
 export class SensorChartComponent implements OnInit {
   sensorId = 'sensor-1';
   chartData: any[] = [];
+  subscription!: Subscription;
 
   view: [number, number] = [700, 300];
 
@@ -27,21 +29,30 @@ export class SensorChartComponent implements OnInit {
   constructor(private sensorService: SensorService) {}
 
   ngOnInit(): void {
+    this.loadChart();
+
+    this.subscription = interval(5000).subscribe(() => {
+      this.loadChart();
+    });
+  }
+
+  loadChart(): void {
     this.sensorService.getHistory(this.sensorId, '6h').subscribe(data => {
-
-      console.log('[SensorChart] Got data:', data);
-
       this.chartData = [
         {
           name: this.sensorId,
           series: data
             .filter(d => !isNaN(d.value))
             .map(d => ({
-            name: new Date(d.timestamp).toLocaleTimeString(),
-            value: d.value
-          }))
+              name: new Date(d.timestamp).toLocaleTimeString(),
+              value: d.value
+            }))
         }
       ];
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
